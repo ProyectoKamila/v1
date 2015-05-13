ea2b992a39611aab99643ad8a5ee43ad1a7779de
sac325 <sac325@gmail.com>
2015-05-12 23:14:31 -0430

    sergioc12052015
    cambios en el insertcontroller email

--- application/controllers/insert_controller.php
+++ application/controllers/insert_controller.php
@@ -4,11 +4,6 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Insert_controller extends MY_Controller {
-    /* public $data = null;
-      public $usuario = null; */
-
-    var $nickname = '';
-    var $status = '';
 
     public function __construct() {
         parent::__construct();
@@ -23,138 +18,181 @@ class Insert_controller extends MY_Controller {
       }elseif($this->session->userdata('status')!=1){
       redirect('./player');
       }
-      } */
+  } */
 
-    public function nuevo() {
+  public function nuevo() {
         //$this->load->view('page/header');
         //$this->load->view('page/insert/insertform');
         //$this->load->view('page/insert/registering');
-        $this->load->view('page/login');
-    }
+    $this->load->view('page/login');
+}
 
-    function recibirDatos() {
+function recibirDatos() {
 
 
-        if (isset($_POST['status']) and $_POST['status'] == '0') {
+    if (isset($_POST['status']) and $_POST['status'] == '0') {
             //SI EXISTE EL CAMPO OCULTO LLAMADO GRABAR CREAMOS LAS VALIDACIONES
             //$this->form_validation->set_rules('nickname','Nombre','required|trim|xss_clean');
-            $this->form_validation->set_rules('email', 'Correo', 'valid_email|required|trim|xss_clean|is_unique[user.email]');
-            $this->form_validation->set_rules('nickname', 'Seudónimo', 'required|trim|xss_clean|min_length[5]|max_length[12]|is_unique[user.nickname]');
-            $this->form_validation->set_rules('pass', 'Clave', 'matches[passc]|md5');
-            $this->form_validation->set_rules('passc', 'Password Confirmation', 'required');
+        $this->form_validation->set_rules('email', 'Correo', 'valid_email|required|trim|xss_clean|is_unique[user.email]');
+        $this->form_validation->set_rules('nickname', 'Seudónimo', 'required|trim|xss_clean|min_length[5]|max_length[12]|is_unique[user.nickname]');
+        $this->form_validation->set_rules('pass', 'Clave', 'matches[passc]|md5');
+        $this->form_validation->set_rules('passc', 'Password Confirmation', 'required');
 
             //SI HAY ALGÚNA REGLA DE LAS ANTERIORES QUE NO SE CUMPLE MOSTRAMOS EL MENSAJE
             //EL COMODÍN %s SUSTITUYE LOS NOMBRES QUE LE HEMOS DADO ANTERIORMENTE, EJEMPLO, 
             //SI EL NOMBRE ESTÁ VACÍO NOS DIRÍA, EL NOMBRE ES REQUERIDO, EL COMODÍN %s 
             //SERÁ SUSTITUIDO POR EL NOMBRE DEL CAMPO
-            $this->form_validation->set_message('required', 'El %s es requerido');
-            $this->form_validation->set_message('valid_email', 'El %s no es válido');
-            $this->form_validation->set_message('is_unique', 'Este %s ya se encuentra en la base de datos');
-            $this->form_validation->set_message('matches', 'La  %s no coincide con la confirmación');
+        $this->form_validation->set_message('required', 'El %s es requerido');
+        $this->form_validation->set_message('valid_email', 'El %s no es válido');
+        $this->form_validation->set_message('is_unique', 'Este %s ya se encuentra en la base de datos');
+        $this->form_validation->set_message('matches', 'La  %s no coincide con la confirmación');
 
             //SI ALGO NO HA IDO BIEN NOS DEVOLVERÁ AL INDEX MOSTRANDO LOS ERRORES
-            if ($this->form_validation->run() == FALSE) {
-                $this->nuevo();
-            } else {
+        if ($this->form_validation->run() == FALSE) {
+            $this->nuevo();
+        } else {
 
-                $correo = $this->input->post('email');
-                $nick = $this->input->post('nickname');
+            $correo = $this->input->post('email');
+            $nick = $this->input->post('nickname');
 
-                $data = array(
-                    'nickname' => $this->input->post('nickname'),
-                    'email' => $this->input->post('email'),
-                    'pass' => $this->input->post('pass'),
-                    'status' => $this->input->post('status')
+            $data = array(
+                'nickname' => $this->input->post('nickname'),
+                'email' => $this->input->post('email'),
+                'pass' => $this->input->post('pass'),
+                'status' => $this->input->post('status')
                 );
-
-
-
-                $this->modelo_universal->insert('user', $data);
-                $this->insertado();
+            if(!$this->enviarcorreo($correo , $nick)){
+                redirect('./nuevo');
             }
+            $this->enviarcorreo($correo , $nick);
+
+            $this->modelo_universal->insert('user', $data);
+            $this->insertado();
         }
     }
+}
 
-    function recibirDc() {
+function recibirDc() {
 
 
-        if (isset($_POST['status']) and $_POST['status'] == '0') {
+    if (isset($_POST['status']) and $_POST['status'] == '0') {
             //SI EXISTE EL CAMPO OCULTO LLAMADO GRABAR CREAMOS LAS VALIDACIONES
             //$this->form_validation->set_rules('nickname','Nombre','required|trim|xss_clean');
-            $this->form_validation->set_rules('first_name', 'Nombre', 'required|trim|xss_clean|min_length[5]|max_length[12]|is_unique[user.nickname]');
-            $this->form_validation->set_rules('last_name', 'Apellido', 'required|trim|xss_clean|min_length[5]|max_length[12]|is_unique[user.nickname]');
-            $this->form_validation->set_rules('identity_card', 'Nº de Identificación', 'required|trim|xss_clean|min_length[5]|max_length[12]|is_unique[user_data.identity_card]');
-            $this->form_validation->set_rules('gender', 'Género', 'required|trim|xss_clean');
-            $this->form_validation->set_rules('date_of_birth', 'Fecha de Nacimiento', 'required|trim|xss_clean');
-            $this->form_validation->set_rules('phone', 'Teléfono', 'required|trim|xss_clean');
-            $this->form_validation->set_rules('nationality', 'Nacionalidad', 'required|trim|xss_clean');
-            $this->form_validation->set_rules('country', 'Pais', 'required|trim|xss_clean');
-            $this->form_validation->set_rules('city', 'Ciudad', 'required|trim|xss_clean');
-            $this->form_validation->set_rules('address', 'Dirección', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('first_name', 'Nombre', 'required|trim|xss_clean|min_length[5]|max_length[12]|is_unique[user.nickname]');
+        $this->form_validation->set_rules('last_name', 'Apellido', 'required|trim|xss_clean|min_length[5]|max_length[12]|is_unique[user.nickname]');
+        $this->form_validation->set_rules('identity_card', 'Nº de Identificación', 'required|trim|xss_clean|min_length[5]|max_length[12]|is_unique[user_data.identity_card]');
+        $this->form_validation->set_rules('gender', 'Género', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('date_of_birth', 'Fecha de Nacimiento', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('phone', 'Teléfono', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('nationality', 'Nacionalidad', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('country', 'Pais', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('city', 'Ciudad', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('address', 'Dirección', 'required|trim|xss_clean');
 
             //SI HAY ALGÚNA REGLA DE LAS ANTERIORES QUE NO SE CUMPLE MOSTRAMOS EL MENSAJE
             //EL COMODÍN %s SUSTITUYE LOS NOMBRES QUE LE HEMOS DADO ANTERIORMENTE, EJEMPLO, 
             //SI EL NOMBRE ESTÁ VACÍO NOS DIRÍA, EL NOMBRE ES REQUERIDO, EL COMODÍN %s 
             //SERÁ SUSTITUIDO POR EL NOMBRE DEL CAMPO
-            $this->form_validation->set_message('required', 'El %s es requerido');
+        $this->form_validation->set_message('required', 'El %s es requerido');
 
-            $this->form_validation->set_message('is_unique', 'Este %s ya se encuentra en la base de datos');
+        $this->form_validation->set_message('is_unique', 'Este %s ya se encuentra en la base de datos');
 
 
             //SI ALGO NO HA IDO BIEN NOS DEVOLVERÁ AL INDEX MOSTRANDO LOS ERRORES
-            if ($this->form_validation->run() == FALSE) {
-                $this->nuevo();
-            } else {
+        if ($this->form_validation->run() == FALSE) {
+            $this->nuevo();
+        } else {
 
 
 
-                $data = array(
-                    'first_name' => $this->input->post('first_name'),
-                    'last_name' => $this->input->post('last_name'),
-                    'identity_card' => $this->input->post('identity_card'),
-                    'gender' => $this->input->post('gender'),
-                    'date_of_birth' => $this->input->post('date_of_birth'),
-                    'phone' => $this->input->post('phone'),
-                    'nationality' => $this->input->post('nationality'),
-                    'country' => $this->input->post('country'),
-                    'city' => $this->input->post('city'),
-                    'address' => $this->input->post('address')
+            $data = array(
+                'first_name' => $this->input->post('first_name'),
+                'last_name' => $this->input->post('last_name'),
+                'identity_card' => $this->input->post('identity_card'),
+                'gender' => $this->input->post('gender'),
+                'date_of_birth' => $this->input->post('date_of_birth'),
+                'phone' => $this->input->post('phone'),
+                'nationality' => $this->input->post('nationality'),
+                'country' => $this->input->post('country'),
+                'city' => $this->input->post('city'),
+                'address' => $this->input->post('address')
                 );
 
 
 
-                $this->modelo_universal->insert('user_data', $data);
-                $this->insertado();
-            }
+            $this->modelo_universal->insert('user_data', $data);
+            $this->insertado();
         }
     }
+}
 
-    public function insertado() {
-        $this->load->view('page/header');
-        $this->load->view('page/insert/insertado');
-    }
+public function insertado() {
+    $this->load->view('page/header');
+    $this->load->view('page/insert/insertado');
+}
 
-    public function insertc() {
+public function insertc() {
         // $this->load->view('page/header');
-        $this->load->view('page/insert/registercompl');
+    $this->load->view('page/insert/registercompl');
+}
+
+public function activar($id = null) {
+
+    if(!$id)
+    {
+        redirect('./nuevo');
     }
 
-    public function activar() {
+    $data = array(
+        'status' => '1'
+        );
 
-        $data['id_user'] = $this->uri->segment(3);
-        $this->nickname = $this->uri->segment(3);
-       // $this->status = $this->uri->segment(3);
-        
-
-        
-       // $this->db->update('user', $this, array('id_user' => $data['id_user']));
+    $this->modelo_universal->update('user', $data, array('id_user' => $id));
 
 
-       // $this->modelo_universal->update('user', $data, $data['nickname']);
+    $this->load->view('page/header');
+    $this->load->view('page/insert/activar',$this->nickname);
+}
+
+function enviarcorreo($correo , $nick)
+{
+    $config = array(
+        'protocol' => 'smtp',
+        'smtp_host' => 'mail.casino4as.com',
+        'smtp_port' => 25,
+        'smtp_user' => 'noreply@casino4as.com',
+        'smtp_pass' => 'Temporal01',
+        'mailtype' => 'html',
+        'charset' => 'utf-8',
+        'newline' => "\r\n"
+        );  
+    $this->email->initialize($config);
 
 
-       $this->load->view('page/header');
-        $this->load->view('page/insert/activar',$this->nickname);
-    }
+
+    $this->email->from('noreply@casino4as.com', 'Casino4As.com');
+    $this->email->to($correo);
+    $this->email->subject('Bienvenido/a a Casino4As.com');
+    $this->email->message('<h2> gracias por registrarte en Casino4As.com</h2><hr><br><br> 
+
+        Tu nombre de usuario es: ' . $nick . '.
+
+        Debes Activar tu Usuario entrando en la sigiente dirección
+
+        localhost/activar/' . $nick . '
+
+        ');
+
+
+
+    
+    if ( $this->email->send() ) {
+       echo "El email fué enviado correctamente!";
+   } else {
+       echo "El email no pudo ser enviado!";
+   }
+
+
+}
 
 }
