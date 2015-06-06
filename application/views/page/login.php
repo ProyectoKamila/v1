514adc007961192d7046e92f62530d9b05901561
jfigueroapcs <jfigueroapcs@hotmail.com>
2015-06-06 16:45:15 -0430

    version de errores
    Sesiones para pa publica de wordpress

--- application/views/page/login.php
+++ application/views/page/login.php
@@ -40,7 +40,7 @@
             <div class="row">
                 <div class="col-md-4 col-md-offset-2">
                     <div class="login-panel panel panel-default">
-                    <? if ($this->session->flashdata('message')!= null){
+                    <?php if ($this->session->flashdata('message')!= null){
                         echo "<div id='infoMessage' class='alert alert-danger' role='alert'>". $this->session->flashdata('message') ."</div>";
                         }
                     ?>
