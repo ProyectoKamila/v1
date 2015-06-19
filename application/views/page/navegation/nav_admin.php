fb61db8ea10f1631291540e22f42cf751eda7636
lisber soto <soto.lmary@gmail.com>
2015-06-19 10:11:44 -0430

    nav admin

--- application/views/page/navegation/nav_admin.php
+++ application/views/page/navegation/nav_admin.php
@@ -54,31 +54,8 @@
                     <li>
                         <a href="<?php echo site_url('activity'); ?>"><i class="fa fa-edit fa-fw"></i>Actividades en casino</a>
                     </li>
-                    <li>
-                        <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="panels-wells.html">Panels and Wells</a>
-                            </li>
-                            <li>
-                                <a href="buttons.html">Buttons</a>
-                            </li>
-                            <li>
-                                <a href="notifications.html">Notifications</a>
-                            </li>
-                            <li>
-                                <a href="typography.html">Typography</a>
-                            </li>
-                            <li>
-                                <a href="icons.html"> Icons</a>
-                            </li>
-                            <li>
-                                <a href="grid.html">Grid</a>
-                            </li>
-                        </ul>
-                        <!-- /.nav-second-level -->
-                    </li>
-                    <li>
+                    
+                   <!-- <li>
                         <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
@@ -102,24 +79,13 @@
                                     <li>
                                         <a href="#">Third Level Item</a>
                                     </li>
-                                </ul>
+                                </ul>-->
                                 <!-- /.nav-third-level -->
-                            </li>
-                        </ul>
+                            <!--</li>
+                        </ul>-->
                         <!-- /.nav-second-level -->
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="blank.html">Blank Page</a>
-                            </li>
-                            <li>
-                                <a href="login.html">Login Page</a>
-                            </li>
-                        </ul>
-                        <!-- /.nav-second-level -->
-                    </li>
+                  <!--  </li>-->
+                    
                 </ul>
             </div>
             <!-- /.sidebar-collapse -->
